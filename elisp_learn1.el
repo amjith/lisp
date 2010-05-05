6efb41dad1d4a63d0e9acd1cd3b88dcec18c313b
Amjith <amjith@localhost.(none)>
2010-05-04 22:56:47 -0600

    Changes upto but excluding page 18 - http://www.lisperati.com/casting-spels-emacs/html/casting-spels-emacs-18.html

--- elisp_learn1.el
+++ elisp_learn1.el
@@ -1,4 +1,7 @@
 (setq eval-expression-print-length nil)
+
+(require 'cl)
+
 (setq objects '(whiskey-bottle bucket frog chain))
 
 (setq map '((living-room (you are in the living room
@@ -22,3 +25,18 @@
 
 (setq location 'living-room)
 
+(defun describe-location (location map)
+  (second (assoc location map)))
+
+(describe-location 'living-room map)   ; Notice the ' in front of living-room but not in front of map. Lisp will treat map as code whereas living-room as data.
+                                       ; So we look up the data "living-room" in the map.
+
+(defun describe-path (path)
+  `(there is a ,(second path) going, (first path) from here -))
+
+(describe-path '(west door garden))
+
+(defun describe-paths (location map)
+  (apply #'append (mapcar #'describe-path (cddr (assoc location map)))))
+
+(describe-paths 'living-room map)
